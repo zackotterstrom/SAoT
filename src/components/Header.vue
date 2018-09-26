b51f1677990d95ed9e6659059300d0cf9b37fd3d
kian00sh <kianoosh.geravesh@itggot.se>
2018-09-26 17:22:11 +0200

    #7 fixed

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    b-jumbotron#header.text-center(
+        bg-variant="info"
+        text-variant="light"
+    )
+        h1(@click="goHome") SAoT
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+export default class Header extends Vue {
+    goHome() {top.location.href = "/";};
+};
+</script>
+
+<style lang="scss">
+#header h1 {
+    cursor: pointer;
+    display: inline;
+}
+</style>
