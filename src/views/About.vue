cbaadc2bff5d7eb4074abaf927e9b6aaff0b439a
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-09-26 12:44:17 +0200

    added boilerplate code

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-jumbotron bg-variant="danger" text-variant="white" border-variant="dark">
+      <template slot="header">
+        Bootstrap Vue
+      </template>
+      <template slot="lead">
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </template>
+      <hr class="my-4">
+      <p>
+        It uses utility classes for typography and spacing to space content
+        out within the larger container.
+      </p>
+    </b-jumbotron>
   </div>
 </template>
