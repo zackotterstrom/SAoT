56d9dfd678d903ec40a590c4e2d81f5e101fe3f9
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-10-09 00:46:10 +0200

    add About.vue

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .container
+    AboutInfo
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AboutInfo from '@/components/AboutInfo.vue';
+
+@Component({
+  components: {
+    AboutInfo
+  },
+})
+export default class Home extends Vue {}
+</script>
