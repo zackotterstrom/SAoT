838a310b9f2de5d84e5130634194b954447914d6
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-10-10 16:46:16 +0200

    update gcloud function

--- src/gcloud/sentiment-analysis.js
+++ src/gcloud/sentiment-analysis.js
@@ -6,6 +6,10 @@
  */
 
 const language = require('@google-cloud/language');
+const {Translate} = require('@google-cloud/translate');
+
+// Creates a client
+const translate = new Translate();
 
 // Instantiates a client
 const client = new language.LanguageServiceClient();
@@ -13,10 +17,23 @@ const client = new language.LanguageServiceClient();
 exports.analyze = (req, res) => {
   let message = req.query.message || req.body.message || '';
   let type = req.query.type || req.body.type || 'sentiment'; // Category/Sentiment/Keyword
+  let lang = req.query.lang || req.body.lang || 'en';
 
   res.set('Access-Control-Allow-Origin', "*");
   res.set('Access-Control-Allow-Methods', 'GET, POST');
+  res.set('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
 
+  if (lang != 'en') {
+  	let translated = translate.translate(message, {from: lang, to: "en"}).then((translated) => {
+      console.log(translated);
+      sendResponse(translated, type, res);
+    })
+  } else {
+    sendResponse(message, type, res);
+  }
+};
+
+function sendResponse(message, type, res) {
   const document = {
     content: message,
     type: 'PLAIN_TEXT',
@@ -75,4 +92,4 @@ exports.analyze = (req, res) => {
       	res.status(500).send(err);
       });
   }
-};
+}
