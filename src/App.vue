01b100d4365fe5df4d8c3045bde751aee4d9cda3
RiskyRusky <zhrikeum@gmail.com>
2018-10-11 13:25:53 +0200

    first push for the footer, have setup the file

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     Header
     #app
       router-view
+    
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 @Component({
   components: {
-    Header
+    Header,
+    Footer
   }
 })
 export default class Home extends Vue {}
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    Footer(color="blue").font-small.pt-4.mt-4
+        container.text-left
+            row
+                colum(sm="6")
+                    h5 Footer Content
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Footer extends Vue {
+    goHome() {
+      this.$router.push("/");
+    };
+
+    goAbout() {
+      this.$router.push("/about");
+    };
+};
+</script>
+
+<style scoped lang="sass">
+
+</style>
